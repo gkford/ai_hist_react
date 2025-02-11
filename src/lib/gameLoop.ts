a152aa50eb8d2622b8ae9449492bd3e1082e4fe0
gkford <gkford@gmail.com>
2025-02-11 13:21:56 +1300

    refactor: Remove unused imports and commented-out code in gameLoop

--- src/lib/gameLoop.ts
+++ src/lib/gameLoop.ts
@@ -1,6 +1,5 @@
 import { useCardsStore } from "@/store/useCardsStore"
 // import { ResourceTransformationProcessor } from "@/components/ui/ResourceTransformation"
-import { allCards } from "@/data/cards"
 
 let intervalId: number | null = null
 
@@ -11,12 +10,9 @@ export function startGameLoop() {
     const cardStates = useCardsStore.getState().cardStates;
 
     Object.keys(cardStates).forEach((cardId) => {
-      const cardDef = allCards.find(c => c.id === cardId);
       const state = cardStates[cardId];
       
-      if (cardDef?.transformation && state?.discovery_state?.current_status === 'discovered') {
-        // ResourceTransformationProcessor.processTransformation(cardId);
-      }
+
     });
   }, 1000);
 }
